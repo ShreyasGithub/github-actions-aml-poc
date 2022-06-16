ad424286423474f9681f32ecab70310f0030e1f2
shreyas <shreyas.suresh@costrategix.com>
2022-06-16 18:12:52 +0530

    data preparation step added

--- code/pipeline_script.py
+++ code/pipeline_script.py
@@ -19,38 +19,38 @@ from azureml.pipeline.core import Pipeline
 
 def main(workspace):
     
-    # config = ScriptRunConfig(source_directory='./code/train',
-    #                          script='train.py',
-    #                          compute_target='github-cluster')
-
-    # # set up pytorch environment
-    # env = Environment.from_conda_specification(
-    #     name='train-env',
-    #     file_path='./code/train/train-env.yml'
-    # )
-    # config.run_config.environment = env
-    
-    # return config
-    
-    run_config = ScriptRunConfig(compute_target='github-cluster')
+    config = ScriptRunConfig(source_directory='./code/data_preparation',
+                             script='data_loader.py',
+                             compute_target='github-cluster')
 
     # set up pytorch environment
     env = Environment.from_conda_specification(
         name='train-env',
         file_path='./code/data_preparation/data_prep_env.yml'
     )
+    config.run_config.environment = env
     
-    run_config.run_config.environment = env
+    return config
     
-    data_prep_step = PythonScriptStep(
-        name="data preparation step",
-        script_name="./code/data_preparation/data_loader.py",
-        source_directory='./code',
-        runconfig=run_config,
-        allow_reuse=True,
-    )
+    # run_config = ScriptRunConfig(compute_target='github-cluster')
+
+    # # set up pytorch environment
+    # env = Environment.from_conda_specification(
+    #     name='train-env',
+    #     file_path='./code/data_preparation/data_prep_env.yml'
+    # )
     
-    return Pipeline(workspace, steps=[data_prep_step])
+    # run_config.run_config.environment = env
+    
+    # data_prep_step = PythonScriptStep(
+    #     name="data preparation step",
+    #     script_name="./code/data_preparation/data_loader.py",
+    #     source_directory='./code',
+    #     runconfig=run_config,
+    #     allow_reuse=True,
+    # )
+    
+    # return Pipeline(workspace, steps=[data_prep_step])
 
 if __name__ == "__main__":
     ws = Workspace.from_config()
