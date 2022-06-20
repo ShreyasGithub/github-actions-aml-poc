d4646e3cd6fbc4e05319277ea90d05937c0abc2c
shreyas <shreyas.suresh@costrategix.com>
2022-06-20 15:16:37 +0530

    model evaluation step added

--- code/evaluation/evaluate.py
+++ code/evaluation/evaluate.py
@@ -4,7 +4,7 @@ from azureml.pipeline.core import PipelineRun
 
 def evaluate_model():
     run = Run.get_context()
-    pipeline_run = PipelineRun(run.experiment)
+    pipeline_run = PipelineRun(run.experiment, run.pipeline_run_id)
     trianing_run = pipeline_run.find_step_run('model training step')
     current_metrics = trianing_run.get_metrics()
     print('current_metrics', current_metrics)
